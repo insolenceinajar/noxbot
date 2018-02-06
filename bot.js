9b149ed673ac03e0b2ab979f073beb750ae1508f
insolenceinajar <36202747+insolenceinajar@users.noreply.github.com>
2018-02-06 18:14:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Bereit!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === '!scream') {
+    	var schrei = Math.floor(Math.random()*3)+1;
+        message.reply(schrei);
   	}
 });
 
-// THIS  MUST  BE  THIS  WAY
+// Token
 client.login(process.env.BOT_TOKEN);
