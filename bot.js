2e1e36ddf15e1cb81db2b80fdc90a4ed739333b2
insolenceinajar <36202747+insolenceinajar@users.noreply.github.com>
2018-02-06 18:36:09 +0100

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+var Discord = require('discord.js');
+var bot = new Discord.Client();
+
+bot.on('ready', () => {
+    console.log('Bereit!');
+});
+
+bot.on('message', message => {
+    if (message.content === '!scream') {
+    	var schrei = Math.floor(Math.random()*3)+1;
+        message.reply(schrei);
+  	}
+});
+
+// Token
+bot.login(process.env.BOT_TOKEN);
