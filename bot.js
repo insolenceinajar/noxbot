0645856ca989b3454e5298ad69b947cc971a782b
insolenceinajar <36202747+insolenceinajar@users.noreply.github.com>
2018-02-06 18:52:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,14 @@ bot.on('ready', () => {console.log('Bereit!');} );
 
 bot.on('message', message => {
     if (message.content === '!scream') {
-    	var schrei = Math.floor(Math.random()*3)+1;
-        message.reply(schrei);
+    	//var schrei = new Array(
+        
+        var FCK = new Array(20);
+        for(var i; i<=20; i++){
+	        var radum = Math.floor(Math.random()*10)+0;
+    	    FCK[i]=radum;
+        }
+        message.reply(FCK);
   	}
 });
 
