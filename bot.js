7e968ff576e93990a32629d0f85875c4487bb30a
insolenceinajar <36202747+insolenceinajar@users.noreply.github.com>
2018-02-06 18:40:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,13 @@
 var Discord = require('discord.js');
 var bot = new Discord.Client();
 
-bot.on('ready', () => {
-    console.log('Bereit!');
-});
+bot.on('ready', () => {console.log('Bereit!');} );
 
 bot.on('message', message => {
     if (message.content === '!scream') {
     	var schrei = Math.floor(Math.random()*3)+1;
         message.reply(schrei);
+        message.sendMessage('Pyu.');
   	}
 });
 
